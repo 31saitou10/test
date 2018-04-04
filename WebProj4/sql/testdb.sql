f8becfb7ba690eaa43efbf0cd2744519932d6aa7
31saitou10 <my-world1231-ma@docomo.ne.jp>
2018-04-04 17:57:42 +0900

    0404

--- WebProj4/sql/testdb.sql
+++ WebProj4/sql/testdb.sql
@@ -11,3 +11,9 @@ password varchar(255)
 insert into users values(1,"taro","123");
 insert into users values(2,"jiro","456");
 insert into users values(3,"hanako","789");
+
+create table inquiry(
+name varchar(255),
+qtype varchar(255),
+body varchar(255)
+);
